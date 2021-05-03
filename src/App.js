da9847c156887de6e120455280e46cb378d2293f
allexib <ooomegasib@gmail.com>
2021-05-03 15:36:16 +0400

    function Test2, func in func

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import './App.css';
 import Test1 from './Test1';
+import Test2 from './Test2';
 import React from 'react';
 
 function App() {
     return (
         <>
-            <Test1 arg={77}/>
+            <Test2 arg={77}/>
         </>
     );
 }
